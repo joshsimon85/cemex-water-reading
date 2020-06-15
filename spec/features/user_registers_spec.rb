367cac820bb4ab1d54f2b8dcf6ea2e35d5f8d545
Josh <joshsimon85@yahoo.com>
2020-06-15 11:09:50 -0600

    Add feature test to check admin is correctly set throgh an invite

--- spec/features/user_registers_spec.rb
+++ spec/features/user_registers_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe 'User registrations process' do
   let(:jon) { create(:user, password: 'password', admin: true) }
   let(:invite) { create(:invitation, user_id: jon.id) }
+  let(:admin_invite) { create(:invitation, user_id: jon.id, admin: true) }
 
   scenario 'registering with out a token', :js => true do
     visit new_user_registration_path
@@ -12,7 +13,7 @@ describe 'User registrations process' do
     expect(page).to have_content('Sign In')
   end
 
-  scenario 'registering with out a token', :js => true do
+  scenario 'registering with a token', :js => true do
     visit "sign_up?token=#{invite.token}"
 
     expect(find_field('Email').value).to eq("#{invite.email}")
@@ -32,5 +33,18 @@ describe 'User registrations process' do
     expect(page).to have_selector('.alert-success')
     expect(page).to have_content('A message with a confirmation link has been sent to your email address')
     expect(page).to have_content('Sign In')
+    expect(User.find_by(email: invite.email).admin).to eq(false)
+  end
+
+  scenario 'registering with an invite with admin set to true', :js => true do
+    visit "sign_up?token=#{admin_invite.token}"
+
+    fill_in 'Full name', with: 'Jane Doe'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign up'
+
+    expect(page).to have_selector('.alert-success')
+    expect(User.find_by(email: admin_invite.email).admin).to eq(true)
   end
 end
