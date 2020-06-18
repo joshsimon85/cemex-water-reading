eec78108a6d873ab6af83d9b6f0431a3ecff80fb
Josh <joshsimon85@yahoo.com>
2020-06-17 23:17:25 -0600

    image zoom

--- app/javascript/packs/zoom_edit.js
+++ app/javascript/packs/zoom_edit.js
@@ -1,5 +1,5 @@
 import { ImageZoom } from './image_zoom';
 
-$(document).on('turbolinks:load', function() {
+$(window).on('turbolinks:load', function() {
   new ImageZoom('.full-size-image', 2).init();
 });
